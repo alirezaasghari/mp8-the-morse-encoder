030b7174536e01e0907c6f0578c6fe4e4d989850
Alireza <alireza@Alirezas-MacBook-Pro.local>
2018-04-13 15:51:47 +0200

    Add project structure

---
+++ src/main/java/at/refugeescode/mp8themorseencoder/Mp8TheMorseEncoderApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp8themorseencoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp8TheMorseEncoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp8TheMorseEncoderApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp8themorseencoder/Mp8TheMorseEncoderApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp8themorseencoder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp8TheMorseEncoderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
